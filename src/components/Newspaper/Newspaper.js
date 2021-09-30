bb1d9609199aeb127249470b1e352deac5994451
Hasan75 <hasanjab14@gmail.com>
2021-09-30 10:53:01 +0600

    progreeser added

--- src/components/Newspaper/Newspaper.js
+++ src/components/Newspaper/Newspaper.js
@@ -13,13 +13,19 @@ const Newspaper = () => {
     },[])
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <Grid  container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {
+                articles.length === 0 ? <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                </Box>
+              :
+              <Grid container spacing={{ xs: 2, md: 3 }}   columns={{ xs: 4, sm: 8, md: 12 }}>
                 {
                     articles.map(article => <Grid item xs={2} sm={4} md={4}>
                         <News article={article}></News>
                      </Grid>)
                 }
             </Grid>
+            }
         </Box>
     );
 };
